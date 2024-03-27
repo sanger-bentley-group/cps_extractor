e333e193bd976a062dad69103041480303133e15
Oliver-Lorenz-dev <oliverlorenz.business@gmail.com>
2024-03-27 14:16:49 +0000

    add new software to versions

--- bin/save_images_info.sh
+++ bin/save_images_info.sh
@@ -7,10 +7,13 @@ find_image () {
 BAKTA=$(find_image bakta)
 BASH=$(find_image bash)
 BLAST=$(find_image blast)
+CHECK_GENE_CONTENT=$(find_image check_gene_content)
 CPS_EXTRACTOR_PYTHON=$(find_image cps_extractor_python)
 GAP_FILLER=$(find_image gap_filler)
+PANAROO=$(find_image panaroo)
 SEROBA=$(find_image seroba)
 SHOVILL=$(find_image shovill)
+SNP_DISTS=$(find_image snp_dists)
 
 add_container () {
     jq -n --arg container "$1" '.container = $container'
@@ -20,8 +23,11 @@ jq -n \
     --argjson bakta "$(add_container "$BAKTA")" \
     --argjson bash "$(add_container "$BASH")" \
     --argjson blast "$(add_container "$BLAST")" \
+    --argjson check_gene_content "$(add_container "$CHECK_GENE_CONTENT")" \
     --argjson cps_extractor_python "$(add_container "$CPS_EXTRACTOR_PYTHON")" \
     --argjson gap_filler "$(add_container "$GAP_FILLER")" \
+    --argjson panaroo "$(add_container "$PANAROO")" \
     --argjson seroba "$(add_container "$SEROBA")" \
     --argjson shovill "$(add_container "$SHOVILL")" \
+    --argjson snp_dists "$(add_container "$SNP_DISTS")" \
     '$ARGS.named' > "$JSON_FILE"
--- bin/save_tools_info.sh
+++ bin/save_tools_info.sh
@@ -10,8 +10,10 @@ jq -n \
     --argjson bedtools "$(add_version "$BEDTOOLS_VERSION")" \
     --argjson blast "$(add_version "$BLAST_VERSION")" \
     --argjson bwa "$(add_version "$BWA_VERSION")" \
+    --argjson panaroo "$(add_version "$PANAROO_VERSION")" \
     --argjson python "$(add_version "$PYTHON_VERSION")" \
     --argjson samtools "$(add_version "$SAMTOOLS_VERSION")" \
     --argjson seroba "$(add_version "$SEROBA_VERSION")" \
     --argjson shovill "$(add_version "$SHOVILL_VERSION")" \
+    --argjson snpdists "$(add_version "$SNPDISTS_VERSION")" \
     '$ARGS.named' > "$JSON_FILE"
